8b34dffc6e6798a41d30c0709edfa49c5a9e544b
Jay / Zsolt T <github@gain.hu>
2022-09-12 17:42:17 +0200

    fix(main): kodbeli hibak javitva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; /* NELEMENTS nincs deklaralva, NELEMENTS != N_ELEMENTS */ 
-    std::cout << '1-100 ertekek duplazasa' /* sor vegerol hianyzik a pontosvesszo */
-    for (int i = 0;) /* ciklus szintaktika hiba  */ 
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << '1-100 ertekek duplazasa';
+    for (int i = 0; i <= 100;++i) 
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) /* nem jo a ciklus, i feltetel! */
+    for (int i = 0; i <= 100; i++)
     {
-        std::cout << "Ertek:" /* nincs pontosvesszo */ /* ill. igy csak a stringet irjuk ki egymas utan sokszor, a tenyleges erteket nem */ 
+        std::cout << "Ertek: " + std::to_string( i ) << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) /* vesszo szerepel pontosvesszo helyett */
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] /* nincs pontosvesszo */
+        atlag += b[i];
     }
-    atlag /= N_ELEMENTS; /* osztando ill oszto felcserelendo */ 
+    atlag = atlag / N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
