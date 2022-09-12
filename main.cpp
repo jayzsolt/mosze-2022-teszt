cc65d43fba32cb12abb1887e82d70737a103a162
Jay / Zsolt T <github@gain.hu>
2022-09-12 17:33:15 +0200

    chore(main): hibak azonositasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; /* NELEMENTS nincs deklaralva, NELEMENTS != N_ELEMENTS */ 
+    std::cout << '1-100 ertekek duplazasa' /* sor vegerol hianyzik a pontosvesszo */
+    for (int i = 0;) /* ciklus szintaktika hiba  */ 
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" /* nincs pontosvesszo */ 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) /* vesszo szerepel pontosvesszo helyett */
     {
-        atlag += b[i]
+        atlag += b[i] /* nincs pontosvesszo */
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
